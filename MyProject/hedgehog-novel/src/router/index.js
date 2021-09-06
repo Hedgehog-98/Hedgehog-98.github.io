a8f8770849ffd90b6d0731af2fe7d8648c19131c
Hedgehog-98 <2275790019@qq.com>
2021-09-06 22:05:52 +0800

    打包项目music

---
+++ MyProject/hedgehog-novel/src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/bookshelf',
+    name: 'BookShelf',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/BookShelf.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes,
+  // mode:'history',//history模式 pushState h5
+})
+
+export default router
